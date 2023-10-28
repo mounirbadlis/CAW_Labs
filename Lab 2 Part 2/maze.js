323f080c5af992ff788b596c1092af47fb26816e
mounirbadlis <mounirbadlis2@gmail.com>
2023-10-28 20:17:24 +0100

    add exercise 7 and some changes

--- Lab 2 Part 2/maze.js
+++ Lab 2 Part 2/maze.js
@@ -1,32 +1,65 @@
-document.addEventListener("DOMContentLoaded", function() {
-const walls = document.querySelectorAll(".boundary");
-const winElement = document.getElementById("end");
-const beginElement = document.getElementById("start");
-//exercise 1
-walls.forEach((wallOnly) =>  {
-    wallOnly.addEventListener("mouseover", function() {
-        wallOnly.style.backgroundColor = 'red';
-    });
-});
+document.addEventListener("DOMContentLoaded", function () {
+    const walls = document.querySelectorAll(".boundary");
+    const winElement = document.getElementById("end");
+    const beginElement = document.getElementById("start");
+    const status = document.getElementById("status");
+    const maze = document.getElementById("maze");
+    const point = document.getElementById("point");
+    var onPlay = false;
 
-//exercise 2
-walls.forEach((wallAll) => {
-    wallAll.addEventListener("mouseover", function() {
-        walls.forEach((wall) => {
-            wall.style.backgroundColor = 'red';
+    //exercise 1
+    walls.forEach((wallOnly) => {
+        wallOnly.addEventListener("mouseover", function () {
+            wallOnly.style.backgroundColor = 'red';
+        });
+    });
+
+    //exercise 2
+    walls.forEach((wallAll) => {
+        wallAll.addEventListener("mouseover", function () {
+            walls.forEach((wall) => {
+                wall.style.backgroundColor = 'red';
+            })
         })
     })
-})
 
-//exercise 3
-winElement.addEventListener("mouseover", function() {
-    window.alert('You Win!')
-})
+    //exercise 3
+    // winElement.addEventListener("mouseover", function() {
+    //     window.alert('You Win!')
+    // })
 
-//exercise 4
-beginElement.addEventListener("click", function() {
-    walls.forEach((wall) => {
-        wall.style.backgroundColor = '#eeeeee';
+    //exercise 4
+    beginElement.addEventListener("click", function () {
+        walls.forEach((wall) => {
+            wall.style.backgroundColor = '#eeeeee';
+        })
+        onPlay = true;
     })
-})
+
+    //exercise 6
+    winElement.addEventListener("mouseover", function () {
+        status.innerHTML = "You Win!"
+    })
+
+    walls.forEach((wallAll) => {
+        wallAll.addEventListener("mouseover", function () {
+            walls.forEach((wall) => {
+                wall.style.backgroundColor = 'red';
+                status.innerHTML = "You Lose"
+            })
+        })
+    })
+
+    //exercise 7
+    document.addEventListener("mouseover", function (event) {
+        if (onPlay) {
+            if (event.target == maze) {
+            } else {
+                walls.forEach((wall) => {
+                    wall.style.backgroundColor = 'red';
+                    status.innerHTML = "You Lose"
+                })
+            }
+        }
+    })
 })
